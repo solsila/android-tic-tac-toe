c4c54197d01304499b57d92a3d1987939dd9929b
Xavi Rigau <xrigau@gmail.com>
2013-08-01 17:34:58 +0100

    refactoring gameboard display test to use mocked display

---
+++ android-tic-tac-toe-core/src/main/java/com/novoda/calisthenics/tictactoe/MyGameBoardDisplay.java
@@ -0,0 +1,11 @@
+package com.novoda.calisthenics.tictactoe;
+
+/**
+* Created by xrigau on 01/08/2013.
+*/
+class MyGameBoardDisplay implements GameBoardDisplay {
+    @Override
+    public void updateCell(Location location, Player player) {
+
+    }
+}
--- android-tic-tac-toe-core/src/test/java/com/novoda/calisthenics/tictactoe/GameBoardTest.java
+++ android-tic-tac-toe-core/src/test/java/com/novoda/calisthenics/tictactoe/GameBoardTest.java
@@ -1,17 +1,31 @@
 package com.novoda.calisthenics.tictactoe;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 public class GameBoardTest {
+
+    @Mock
+    GameBoardDisplay gameBoardDisplay;
+
+
+    @Before
+    public void setUp() throws Exception {
+        initMocks(this);
+    }
 
     @Test
     public void showTheBoardOnADisplay() throws Exception {
         GameBoard board = new GameBoard();
-        board.display(new GameBoardDisplay() {
-            @Override
-            public void updateCell(Location location, Player player) {
-                
-            }
-        });
+        board.display(gameBoardDisplay);
+        
+        verify(gameBoardDisplay, atLeastOnce()).updateCell(any(Location.class), any(Player.class));
     }
+
 }
